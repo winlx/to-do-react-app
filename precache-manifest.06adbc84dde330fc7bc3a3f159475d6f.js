34f1a630bf3bbcb7128267f4593237d75e7b284c
Aleksei Iakovets <aleksei@winlx.com>
2019-07-20 23:55:32 +0300

    Updates

---
+++ precache-manifest.06adbc84dde330fc7bc3a3f159475d6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "712a1a406c211dbfdb4bf52045010851",
+    "url": "/to-do-react-app/index.html"
+  },
+  {
+    "revision": "3fb16b341c4e040c8435",
+    "url": "/to-do-react-app/static/css/2.3f182d76.chunk.css"
+  },
+  {
+    "revision": "6a1cdf14f5e68dfaa5b7",
+    "url": "/to-do-react-app/static/css/main.edb892bc.chunk.css"
+  },
+  {
+    "revision": "3fb16b341c4e040c8435",
+    "url": "/to-do-react-app/static/js/2.f1b50c39.chunk.js"
+  },
+  {
+    "revision": "6a1cdf14f5e68dfaa5b7",
+    "url": "/to-do-react-app/static/js/main.4d5c79eb.chunk.js"
+  },
+  {
+    "revision": "7a1c1d7cd92a016aceb5",
+    "url": "/to-do-react-app/static/js/runtime~main.250cd15c.js"
+  }
+]);
