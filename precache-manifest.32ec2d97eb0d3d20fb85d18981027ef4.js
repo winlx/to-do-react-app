97f7f0c9e203f7bf1ad5728f2444560e8ecccf32
Aleksei Iakovets <aleksei@winlx.com>
2019-07-21 00:43:34 +0300

    Updates

--- precache-manifest.32ec2d97eb0d3d20fb85d18981027ef4.js
+++ precache-manifest.32ec2d97eb0d3d20fb85d18981027ef4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "815a7bfa37650a99b65b56407dc05559",
+    "revision": "0438ab5ff1a9e3e99e25c0a045f781ae",
     "url": "/to-do-react-app/index.html"
   },
   {
-    "revision": "4ea08eec0b4c57383ba7",
+    "revision": "3fb16b341c4e040c8435",
     "url": "/to-do-react-app/static/css/2.3f182d76.chunk.css"
   },
   {
-    "revision": "f32d5686ff67af05d490",
+    "revision": "0ffd879783af01f515b1",
     "url": "/to-do-react-app/static/css/main.edb892bc.chunk.css"
   },
   {
-    "revision": "4ea08eec0b4c57383ba7",
-    "url": "/to-do-react-app/static/js/2.d2810db8.chunk.js"
+    "revision": "3fb16b341c4e040c8435",
+    "url": "/to-do-react-app/static/js/2.f1b50c39.chunk.js"
   },
   {
-    "revision": "f32d5686ff67af05d490",
-    "url": "/to-do-react-app/static/js/main.eb4f22c3.chunk.js"
+    "revision": "0ffd879783af01f515b1",
+    "url": "/to-do-react-app/static/js/main.88ca73a6.chunk.js"
   },
   {
     "revision": "7a1c1d7cd92a016aceb5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/to-do-react-app/precache-manifest.f1ea174fc016239fcffbc763551ba569.js"
+  "/to-do-react-app/precache-manifest.32ec2d97eb0d3d20fb85d18981027ef4.js"
 );
 
 self.addEventListener('message', (event) => {
